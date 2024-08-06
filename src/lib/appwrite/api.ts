099e0e7ce9580471da566d940a7b306e6ef004eb
Jake Wright <jake.wright99@yahoo.com>
2024-08-06 21:35:33 +0100

    added explore page

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,5 +1,5 @@
 import { ID, Query } from 'appwrite';
-import { INewPost, INewUser } from "@/types";
+import { INewPost, INewUser, IUpdatePost } from "@/types";
 import { account, appwriteConfig, avatars, databases, storage } from './config';
 import React from 'react';
 
@@ -263,6 +263,115 @@ export async function deleteSavedPost(savedRecordId: string) {
     console.log(error)
   }
 }
+
+export async function getPostById(postId: string) {
+  try {
+    const post = await databases.getDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.postsCollectionId,
+      postId
+    )
+
+    return post;
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function UpdatePost(post: IUpdatePost) {
+  const hasFileToUpdate = post.file.length > 0;
+  try {
+    let image = {
+      imageUrl: post.imageUrl,
+      imageId: post.imageId,
+    }
+
+    if(hasFileToUpdate) {
+      // Upload image to storage
+      const uploadedFile = await uploadFile(post.file[0]);
+      if(!uploadedFile) throw Error;
+
+      //get file url
+    const fileUrl = getFilePreview(uploadedFile.$id);
+
+    if(!fileUrl) {
+      deleteFile(uploadedFile.$id);
+      throw Error;
+     }
+
+     image = { ...image, imageUrl: fileUrl, imageId: uploadedFile.$id  }
+    }
+
+    
+
+    //convert tags into an array
+    const tags = post.tags?.replace(/ /g, '').split(',') || [];
+
+    //save post to datababse
+    const updatedPost = await databases.updateDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.postsCollectionId,
+      post.postId,
+      {
+        caption:post.caption,
+        imageUrl: image.imageUrl,
+        imageId: image.imageId,
+        location: post.location,
+        tags: tags
+      }
+    )
+
+    if(!updatedPost) {
+      await deleteFile(post.imageId)
+      throw Error;
+    }
+
+    return updatedPost;
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function deletePost(postId: string, imageId: string) {
+  if(!postId || !imageId) throw Error;
+
+  try {
+    await databases.deleteDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.postsCollectionId,
+      postId
+    )
+
+    return { status: 'ok' }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function getInfinitePosts({ pageParam}: { pageParam: number}) {
+  const queries: any[] = [Query.orderDesc('$updatedAt'), Query.limit(10)]
+
+  if(pageParam) {
+    queries.push(Query.cursorAfter(pageParam.toString()));
+  }
+
+  try {
+    const posts = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.postsCollectionId,
+      queries
+    )
+
+    if(!posts) throw Error;
+
+    return posts;
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+
 
 
 
